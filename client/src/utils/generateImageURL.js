1ec3aef4d87ef2e4a0bebcc2381cc44d30c777fe
rakib38324 <aminul.rakib2022@gmail.com>
2024-09-11 17:11:19 +0600

    update registration and login system

--- client/src/utils/generateImageURL.js
+++ client/src/utils/generateImageURL.js
@@ -3,12 +3,14 @@ import axios from "axios";
 const generateImageURL = async (image) => {
   const file = new FormData();
   file.append("file", image);
-  file.append("upload_preset", process.env.CLOUDINARY_PRESET);
+  file.append("upload_preset", import.meta.env.VITE_CLOUDINARY_PRESET);
 
+  
   const { data } = await axios.post(
-    `https://api.cloudinary.com/v1_1/${process.env.CLOUDINARY_ENV}/image/upload`,
+    `https://api.cloudinary.com/v1_1/${import.meta.env.VITE_CLOUDINARY_NAME}/image/upload`,
     file
   );
+
   return data;
 };
 
