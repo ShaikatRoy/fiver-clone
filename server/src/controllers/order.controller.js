41a7f7b866a0fd46f27f076c63dc2db902437495
rakib38324 <aminul.rakib2022@gmail.com>
2024-09-16 20:36:37 +0600

    update buyser business logic, like seller can not order with gigs

--- server/src/controllers/order.controller.js
+++ server/src/controllers/order.controller.js
@@ -4,7 +4,7 @@ const stripe = require('stripe')(process.env.STRIPE_SECRET);
 
 const getOrders = async (request, response) => {
     try {
-        const orders = await Order.find({ $and: [{ $or: [{ sellerID: request.userID }, { buyerID: request.userID }] }, { isCompleted: true }] }).populate(request.isSeller? 'buyerID' : 'sellerID', 'username email image country');
+        const orders = await Order.find({ $and: [{ $or: [{ sellerID: request.userID }, { buyerID: request.userID }] }, { isCompleted: true }] }).populate(request.isSeller ? 'buyerID' : 'sellerID', 'username email image country');
         return response.send(orders);
     }
     catch ({ message, status = 500 }) {
@@ -20,7 +20,22 @@ const paymentIntent = async (request, response) => {
 
     try {
         const gig = await Gig.findOne({ _id });
-        const customerInformation = await User.findById({_id: gig?.userID})
+        const customerInformation = await User.findById({ _id: request.userID })
+
+    
+        if (!customerInformation) {
+            return response.send({
+                error: true,
+                message: "Buyer information not found."
+            })
+        }
+
+        if (customerInformation?.isSeller === true) {
+            return response.send({
+                error: true,
+                message: "Your are a seller, Seller Can't Order with gigs."
+            })
+        }
 
         const payment_intent = await stripe.paymentIntents.create({
             amount: gig.price * 100,
@@ -32,12 +47,12 @@ const paymentIntent = async (request, response) => {
             shipping: {
                 name: customerInformation?.username,          // Pass the customer's name
                 address: {
-                  line1: 'Address Line 1',   // Address fields
-                  city: 'City',
-                  postal_code: 'IN',
-                  country: "IN",             // Country code (India in this case)
+                    line1: 'Address Line 1',   // Address fields
+                    city: 'City',
+                    postal_code: 'IN',
+                    country: "IN",             // Country code (India in this case)
                 },
-              },
+            },
         });
 
         const order = new Order({
@@ -57,7 +72,7 @@ const paymentIntent = async (request, response) => {
         })
 
     }
-    catch({message, status = 500}) {
+    catch ({ message, status = 500 }) {
         return response.send({
             error: true,
             message
@@ -75,7 +90,7 @@ const updatePaymentStatus = async (request, response) => {
             }
         }, { new: true });
 
-        if(order?.isCompleted) {
+        if (order?.isCompleted) {
             return response.status(202).send({
                 error: false,
                 message: 'Order has been confirmed!'
@@ -84,7 +99,7 @@ const updatePaymentStatus = async (request, response) => {
 
         throw CustomException('Payment status not updated!', 500);
     }
-    catch({message, status = 500}) {
+    catch ({ message, status = 500 }) {
         return response.status(status).send({
             error: true,
             message
